64bb708ce3e45ed900ea7044eef656231cb11375
Alexey <alexeyradomskiy98@gmail.com>
2022-04-01 18:23:29 +0300

    Initial Commit

---
+++ SwiftUITrafficLight/SwiftUITrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITrafficLightApp.swift
+//  SwiftUITrafficLight
+//
+//  Created by Alexey on 01.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
